a25f22cd8f634b2bcf5f73c6899f502f416b8069
HuyQuang-hrz <dtc21h4801030084@ictu.edu.vn>
2026-02-05 15:29:11 +0700

    api authors

---
+++ app/schemas/author.py
@@ -0,0 +1,25 @@
+from typing import Optional
+from pydantic import BaseModel
+
+class AuthorBase(BaseModel):
+    name:str
+    bio: Optional[str] = None
+
+class AuthorCreate(AuthorBase):
+    """Schema for create author"""
+    pass
+
+class AuthorUpdate(BaseModel):
+    """Schema for update author"""
+    name: Optional[str] = None
+    bio: Optional[str] = None
+
+class AuthorInDBBase(AuthorBase):
+    id: int
+
+    class Config:
+        orm_mode = True #Pydantic read from SQLAlchemy model
+
+class Author(AuthorInDBBase):
+    """Schema return for client"""
+    pass
